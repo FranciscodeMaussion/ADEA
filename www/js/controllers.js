1a124516c7f4b3afb2fbe5fb4a07a5b0b08f2f56
frandemo <franciscodemaussion@gmail.com>
2016-09-22 08:26:12 -0300

    add fromly

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,17 +2,17 @@ angular.module('app.controllers', [])
 
 .controller('inventarioCtrl', ['$scope', '$stateParams', '$state', '$rootScope', function ($scope, $stateParams, $state, $rootScope) {
     $rootScope.categorias = [
-      {'id': 0,'nombre':'Tierras','valor':-1},// Cantidad de HAS, precio unitario y precio total nada mas
-      {'id': 1,'nombre':'Mejoras','valor':5},// defecto
-      {'id': 2,'nombre':'Tractores, Maquinarias y Rodados','valor':10}, //defecto
-      {'id': 3,'nombre':'Pasturas Perennes','valor':1},// valor unitario, superficie, calculado costo total, año implantacion,
-      {'id': 4,'nombre':'Reproductores y Animales de Trabajo','valor':10}, // sin estado, valor nuevo por valor unitario, nuevo calculado total
-      {'id': 5,'nombre':'Deposito','valor':0},
-      {'id': 6,'nombre':'Granos Almacenados','valor':0},
-      {'id': 7,'nombre':'Ganado Circulante','valor':0},
-      {'id': 8,'nombre':'Ganado de Renta','valor':0},
-      {'id': 9,'nombre':'Pasturas Anuales','valor':-1},
-      {'id': 10,'nombre':'Cultivos Anuales','valor':-1}
+      {'id': 0,'nombre':'Tierras','valor':-1, 'do':'T'},// Tierras
+      {'id': 1,'nombre':'Mejoras','valor':5, 'do':'V'},// con vraci
+      {'id': 2,'nombre':'Tractores, Maquinarias y Rodados','valor':10, 'do':'V'}, // con vraci
+      {'id': 3,'nombre':'Pasturas Perennes','valor':1, 'do':'A'},// con vraci
+      {'id': 4,'nombre':'Reproductores y Animales de Trabajo','valor':10, 'do':'V'}, // con vraci
+      {'id': 5,'nombre':'Deposito','valor':0, 'do':'CyF'}, // sin vraci
+      {'id': 6,'nombre':'Granos Almacenados','valor':0, 'do':'CyF'},// sin vraci
+      {'id': 7,'nombre':'Ganado Circulante','valor':0, 'do':'CyF'},// sin vraci
+      {'id': 8,'nombre':'Ganado de Renta','valor':0, 'do':'CyF'},// sin vraci
+      {'id': 9,'nombre':'Pasturas Anuales','valor':-1, 'do':'CyF'},// sin vraci
+      {'id': 10,'nombre':'Cultivos Anuales','valor':-1, 'do':'CyF'}// sin vraci
     ];
     $scope.$on('$ionicView.beforeEnter', function () {
       $rootScope.categorySelected = $rootScope.categorias[0];
@@ -87,26 +87,72 @@ angular.module('app.controllers', [])
     var a = $scope.item;
     var hoy = new Date().getFullYear();
     var year = a.bought.getFullYear();
-    var dfp = a.life - ( hoy - year );
-    var vr = (a.cat.valor*a.valnew)/100
-    if (dfp < 0){
-      dfp = 0;
+    var cat = JSON.parse(a.cat);
+    if (a.cat.do == 'V'){
+      var dfp = a.life - ( hoy - year );
+      var vr = (a.cat.valor*a.valnew)/100
+      if (dfp < 0){
+        dfp = 0;
+      }
+      var vraci;
+      var cad;
+      if (dfp !== 0){
+        vraci = ((a.valnew - vr)*dfp/a.life +vr)* a.cant;
+        cad = (vraci-vr)/dfp
+      }else{
+        vraci = vr;
+        cad = 0;
+      }
+      $scope.calculados = {
+        'dfp': dfp,
+        'vr': vr,
+        'vraci': vraci,
+        'cad': cad,
+      };
+      if (a.cat.id == 4){
+        $scope.calculados = {
+          'dfp': dfp,
+          'vr': vr,
+          'vraci': vraci,
+          'cad': cad,
+          'tot':a.new*a.cant
+        };
+      }
+    }else if (a.cat.do == 'T') {
+      $scope.calculados = {
+        'tot': a.cant*a.prec,
+      };
+    }else if (a.cat.do == 'A') {
+      var dfp = a.life - ( hoy - year );
+      if (dfp < 0){
+        dfp = 0;
+      }
+      var vraci;
+      var cad;
+      var tot = a.cost*a.sup
+      if (dfp !== 0){
+        vraci = (tot*dfp)/a.life;
+        cad = vraci/dfp
+      }else{
+        vraci = 0;
+        cad = 0;
+      }
+      $scope.calculados = {
+        'dfp': dfp,
+        'tot': tot,
+        'vraci': vraci,
+        'cad': cad,
+      };
+    }else if (a.cat.do == 'CyF') {
+      var tot1 = a.cant1*a.new1
+      var tot2 = a.cant2*a.new2
+      $scope.calculados = {
+        'tot1': tot1,
+        'tot2': tot2,
+        'totfin': tot2-tot1,
+      };
     }
-    var vraci;
-    var cad;
-    if (dfp !== 0){
-      vraci = ((a.valnew - vr)*dfp/a.life +vr)* a.cant;
-      cad = (vraci-vr)/dfp
-    }else{
-      vraci = vr;
-      cad = 0;
-    }
-    $scope.calculados = {
-      'dfp': dfp,
-      'vr': vr,
-      'vraci': vraci,
-      'cad': cad,
-    };
+
   });
   $scope.modify = function(){
     $state.go('tabsController.modItem', {modItem: $stateParams.itemId})
